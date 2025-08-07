53148d8c24847a4e08eefeae61698147d0c1dbf1
AtrCheema <ather_abbas786@yahoo.com>
2025-08-07 13:59:01 +0000

    added data preparation for timeseries

---
+++ examples/preparing_data_for_ts_prediction.py
@@ -0,0 +1,392 @@
+"""
+============================================
+Data Preparation for Time Series Prediction
+============================================
+
+This example demonstrates how to prepare data for time series prediction especially for
+deep learning models/algorithms like LSTM/RNN.
+
+"""
+
+import numpy as np
+np.__version__
+
+from utils import prepare_data
+# %%
+# Here we create a simple dataset with 2000 rows and 1 columns i.e. a
+# univariate time series with no covariates.
+
+rows = 2000
+cols = 1
+data = np.arange(int(rows*cols)).reshape(-1,rows).transpose()
+
+# %%
+# Below we print the first 10 rows, the shape of the dataset, and the last 10 rows to give an overview
+# of the data structure.
+
+print(data[0:10])  
+print('\n {} \n'.format(data.shape))
+print(data[-10:])
+x, _y, y = prepare_data(data, num_inputs=1, num_outputs=1, lookback=4)
+
+print(x.shape, _y.shape, y.shape)
+
+# %%
+# Here we create a simple dataset with 2000 rows and 6 columns. Each column can represent
+# a different feature or variable in the time series data. The dataset is filled with sequential
+# integers for demonstration purposes.
+rows = 2000
+cols = 6
+data = np.arange(int(rows*cols)).reshape(-1,rows).transpose()
+print(data[0:10])  
+print('\n {} \n'.format(data.shape))
+print(data[-10:])
+
+
+# %%
+# multivariate time series with no covariates
+
+x, _y, y = prepare_data(data, num_inputs=6, num_outputs=6, lookback=4)
+
+print(x.shape, _y.shape, y.shape)
+
+# %%
+x, _y, y = prepare_data(data, num_inputs=5, lookback=4)
+
+print(x.shape, _y.shape, y.shape)
+
+# %%
+
+x[0]
+
+# %%
+
+_y[0]
+
+# %%
+
+y[0]
+
+# %%
+
+x[1]
+
+# %%
+
+_y[1]
+
+# %%
+
+y[1]
+
+# %%
+
+x, _y, y = prepare_data(data, num_inputs=4, lookback=4)
+
+print(x.shape, _y.shape, y.shape)
+
+# %%
+
+x[0]
+
+# %%
+
+_y[0]
+
+# %%
+
+y[0]
+
+# %%
+
+x[1]
+
+# %%
+
+_y[1]
+
+# %%
+
+y[1]
+
+# %%
+# nowcasting vs forecasting
+# --------------------------
+
+x, _y, y = prepare_data(data, num_inputs=5, lookback=4, forecast_step=1)
+
+print(x.shape, _y.shape, y.shape)
+
+# %%
+
+x[0]
+
+# %%
+
+_y[0]
+
+# %%
+
+y[0]
+
+# %%
+
+x[1]
+
+# %%
+
+_y[1]
+
+# %%
+
+y[1]
+
+# %%
+# if we want to forecast multiple timesteps in future
+
+x, _y, y = prepare_data(data, num_inputs=5, lookback=4, forecast_step=1, forecast_len=2)
+
+print(x.shape, _y.shape, y.shape)
+
+# %%
+
+x[0]
+
+# %%
+
+_y[0]
+
+# %%
+
+y[0]
+
+# %%
+
+x[1]
+
+# %%
+
+_y[1]
+
+# %%
+
+y[1]
+
+# %%
+
+x, _y, y = prepare_data(data, num_inputs=5, lookback=4, forecast_step=0, forecast_len=2)
+
+print(x.shape, _y.shape, y.shape)
+
+# %%
+
+x[0]
+
+# %%
+
+_y[0]
+
+# %%
+
+y[0]
+
+# %%
+
+x[1]
+
+# %%
+
+_y[1]
+
+# %%
+
+y[1]
+
+# %%
+x, _y, y = prepare_data(data, num_inputs=5, lookback=1, forecast_step=0)
+
+# %%
+# chaning input_steps
+x, _y, y = prepare_data(data, num_inputs=5, lookback=4, input_steps=2)
+
+print(x.shape, _y.shape, y.shape)
+
+# %%
+
+x[0]
+
+# %%
+
+_y[0]
+
+# %%
+
+y[0]
+
+# %%
+
+x[1]
+
+# %%
+
+_y[1]
+
+# %%
+
+y[1]
+
+
+# %%
+# chaning output_steps
+x, _y, y = prepare_data(data, num_inputs=5, lookback=4, output_steps=2)
+
+print(x.shape, _y.shape, y.shape)
+
+# %%
+
+x[0]
+
+# %%
+
+_y[0]
+
+# %%
+
+y[0]
+
+# %%
+
+x[1]
+
+# %%
+
+_y[1]
+
+# %%
+
+y[1]
+
+# %%
+# Handling missing values
+# --------------------------
+# missing values in the output
+
+data = np.arange(int(rows*cols)).reshape(-1,rows).transpose()
+rng = np.random.default_rng(seed=313)  # for reproducibility
+# create a random mask for the last column
+mask = rng.integers(0, 2, size=data[:, -1].shape).astype(bool)
+# introduce NaNs in the last column
+data = data.astype(float)
+data[mask, -1] = None
+
+print(data[0:10])  
+print('\n {} \n'.format(data.shape))
+print(data[-10:])
+
+# %%
+
+x, _y, y = prepare_data(data, num_inputs=5, lookback=4)
+print(x.shape, _y.shape, y.shape)
+
+
+# %%
+
+y[0]
+
+# %%
+
+y[1]
+
+# %%
+
+y[2]
+
+# %%
+y[3]
+
+# %%
+y[4], y[5], y[6]
+
+# %%
+# removing all examples with NaN in the output
+nan_idx_y = np.isnan(y).any(axis=(1, 2))
+
+non_nan_idx_y = np.invert(nan_idx_y)
+
+x = x[non_nan_idx_y]
+_y = _y[non_nan_idx_y]
+y = y[non_nan_idx_y]
+
+print(x.shape, _y.shape, y.shape)
+
+# missing values in the input
+data = np.arange(int(rows*cols)).reshape(-1,rows).transpose()
+rng = np.random.default_rng(seed=313)  # for reproducibility
+# put missing at random positions in the input data
+mask = rng.integers(0, 50, size=data[:, :-1].shape).astype(bool)
+data = data.astype(float)
+data[:, :-1][~mask] = np.nan
+
+print(data[0:10])  
+print('\n {} \n'.format(data.shape))
+print(data[-10:])
+
+# %%
+x, _y, y = prepare_data(data, num_inputs=5, lookback=5)
+print(x.shape, _y.shape, y.shape)
+
+x[-3]
+
+# %%
+
+x[-4]
+
+# %%
+
+y[-4]
+
+# %%
+# removing all examples with NaN in the input
+
+nan_idx_x = np.isnan(x).any(axis=(1, 2))
+
+non_nan_idx_x = np.invert(nan_idx_x)
+
+x = x[non_nan_idx_x]
+_y = _y[non_nan_idx_x]
+y = y[non_nan_idx_x]
+
+print(x.shape, _y.shape, y.shape)
+
+
+# %%
+# making batches
+# --------------
+lookback = 4
+num_inputs = 5
+data = np.arange(int(rows*cols)).reshape(-1,rows).transpose()
+x, _y, y = prepare_data(data, num_inputs=num_inputs, lookback=lookback)
+print(x.shape, _y.shape, y.shape)
+
+from tensorflow.keras.layers import Input, LSTM, Dense
+from tensorflow.keras.models import Model
+
+inputs = Input(shape=(lookback, num_inputs))
+lstm = LSTM(32)(inputs)
+output = Dense(1)(lstm)
+model = Model(inputs=inputs, outputs=output)
+
+model.compile(optimizer='adam', loss='mse')
+
+model.fit(x, y, epochs=2, batch_size=128)
+
+pred = model.predict(x)
+
+# %%
+# using generator
+# ---------------
+# In previous example, we had 1997 samples/examples, and each sample had shape (4, 5).
+# This is small data and we can fit it in memory.
+# But in real world, we may have large datasets with e.g. millions of samples/examples
+# that cannot fit in memory.
+# In such cases, we can use a data generator to load and preprocess the data in batches ourselves.
---
+++ examples/utils.py
@@ -0,0 +1,384 @@
+
+from typing import Union, Tuple
+
+import numpy as np
+
+
+# todo : when inputs and outputs are same
+# todo : make use of output_steps
+
+def prepare_data(
+        data: np.ndarray,
+        lookback: int,
+        num_inputs: int = None,
+        num_outputs: int = None,
+        input_steps: int = 1,
+        forecast_step: int = 0,
+        forecast_len: int = 1,
+        known_future_inputs: bool = False,
+        output_steps: int = 1,
+        mask: Union[int, float, np.ndarray] = None
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    converts a numpy nd array into a supervised machine learning problem.
+
+    Parameters
+    ----------
+        data :
+            nd numpy array whose first dimension represents the number
+            of examples and the second dimension represents the number of features.
+            Some of those features will be used as inputs and some will be considered
+            as outputs depending upon the values of `num_inputs` and `num_outputs`.
+        lookback :
+            number of previous steps/values to be used at one step.
+        num_inputs :
+            default None, number of input features in data. If None,
+            it will be calculated as features-outputs. The input data will be all
+            from start till num_outputs in second dimension.
+        num_outputs :
+            number of columns (from last) in data to be used as output.
+            If None, it will be caculated as features-inputs.
+        input_steps:
+            strides/number of steps in input data
+        forecast_step :
+            must be greater than equal to 0, which t+ith value to
+            use as target where i is the horizon. For time series prediction, we
+            can say, which horizon to predict.
+        forecast_len :
+            number of horizons/future values to predict.
+        known_future_inputs :
+            Only useful if `forecast_len`>1. If True, this
+            means, we know and use 'future inputs' while making predictions at t>0
+        output_steps :
+            step size in outputs. If =2, it means we want to predict
+            every second value from the targets
+        mask :
+            If int, then the examples with these values in
+            the output will be skipped. If array then it must be a boolean mask
+            indicating which examples to include/exclude. The length of mask should
+            be equal to the number of generated examples. The number of generated
+            examples is difficult to prognose because it depend, upon lookback, input_steps,
+            and forecast_step. Thus, it is better to provide an integer indicating
+            which values in outputs are to be considered as invalid. Default is
+            None, which indicates all the generated examples will be returned.
+
+    Returns
+    -------
+        x : numpy array of shape (examples, lookback, ins) consisting of
+            input examples
+        prev_y : numpy array consisting of previous outputs
+        y : numpy array consisting of target values of shape (examples, outs, forecast_len)
+
+    Given following data consisting of input/output pairs
+
+    +--------+--------+---------+---------+----------+
+    | input1 | input2 | output1 | output2 | output 3 |
+    +========+========+=========+=========+==========+
+    |   1    |   11   |   21    |    31   |   41     |
+    +--------+--------+---------+---------+----------+
+    |   2    |   12   |   22    |    32   |   42     |
+    +--------+--------+---------+---------+----------+
+    |   3    |   13   |   23    |    33   |   43     |
+    +--------+--------+---------+---------+----------+
+    |   4    |   14   |   24    |    34   |   44     |
+    +--------+--------+---------+---------+----------+
+    |   5    |   15   |   25    |    35   |   45     |
+    +--------+--------+---------+---------+----------+
+    |   6    |   16   |   26    |    36   |   46     |
+    +--------+--------+---------+---------+----------+
+    |   7    |   17   |   27    |    37   |   47     |
+    +--------+--------+---------+---------+----------+
+
+    If we use following 2 time series as input
+
+    +--------+--------+
+    | input1 | input2 |
+    +========+========+
+    |  1     |  11    |
+    +--------+--------+
+    |     2  |  12    |
+    +--------+--------+
+    | 3      |  13    |
+    +--------+--------+
+    | 4      |  14    |
+    +--------+--------+
+    | 5      |  15    |
+    +--------+--------+
+    | 6      |  16    |
+    +--------+--------+
+    | 7      |  17    |
+    +--------+--------+
+
+    then  ``num_inputs`` =2, ``lookback`` =7, ``input_steps`` =1
+
+    and if we want to predict
+
+    +---------+---------+----------+
+    | output1 | output2 | output 3 |
+    +=========+=========+==========+
+    |   27    |   37    |   47     |
+    +---------+---------+----------+
+
+    then ``num_outputs`` =3, ``forecast_len`` =1,  ``forecast_step`` =0,
+
+    if we want to predict
+
+    +---------+---------+----------+
+    | output1 | output2 | output 3 |
+    +=========+=========+==========+
+    | 28      | 38      | 48       |
+    +---------+---------+----------+
+
+    then ``num_outputs`` =3, ``forecast_len`` =1,  ``forecast_step`` =1,
+
+    if we want to predict
+
+    +---------+---------+----------+
+    | output1 | output2 | output 3 |
+    +=========+=========+==========+
+    |  27     |  37     |  47      |
+    +---------+---------+----------+
+    |  28     |  38     |  48      |
+    +---------+---------+----------+
+
+    then ``num_outputs`` =3, ``forecast_len`` =2,  horizon/forecast_step=0,
+
+    if we want to predict
+
+    +---------+---------+----------+
+    | output1 | output2 | output 3 |
+    +=========+=========+==========+
+    |   28    |   38    |   48     |
+    +---------+---------+----------+
+    |   29    |   39    |   49     |
+    +---------+---------+----------+
+    |   30    |   40    |   50     |
+    +---------+---------+----------+
+
+    then ``num_outputs`` =3, ``forecast_len`` =3,  ``forecast_step`` =1,
+
+    if we want to predict
+
+    +---------+
+    | output2 |
+    +=========+
+    |   38    |
+    +---------+
+    |   39    |
+    +---------+
+    |   40    |
+    +---------+
+
+    then ``num_outputs`` =1, ``forecast_len`` =3, ``forecast_step`` =0
+
+    if we predict
+
+    +---------+
+    | output2 |
+    +=========+
+    | 39      |
+    +---------+
+
+    then ``num_outputs`` =1, ``forecast_len`` =1, ``forecast_step`` =2
+
+    if we predict
+
+    +---------+
+    | output2 |
+    +=========+
+    | 39      |
+    +---------+
+    | 40      |
+    +---------+
+    | 41      |
+    +---------+
+
+     then ``num_outputs`` =1, ``forecast_len`` =3, ``forecast_step`` =2
+
+    If we use following two time series as input
+
+    +--------+--------+
+    |input1  | input2 |
+    +========+========+
+    |   1    |  11    |
+    +--------+--------+
+    |   3    |  13    |
+    +--------+--------+
+    |   5    |  15    |
+    +--------+--------+
+    |   7    |  17    |
+    +--------+--------+
+
+    then   ``num_inputs`` =2, ``lookback`` =4, ``input_steps`` =2
+
+    If the input is
+
+    +--------+--------+
+    | input1 | input2 |
+    +========+========+
+    |    1   |  11    |
+    +--------+--------+
+    |    2   |  12    |
+    +--------+--------+
+    |    3   |  13    |
+    +--------+--------+
+    |    4   |  14    |
+    +--------+--------+
+    |    5   |  15    |
+    +--------+--------+
+    |    6   |  16    |
+    +--------+--------+
+    |   7    |  17    |
+    +--------+--------+
+
+    and target/output is
+
+    +---------+---------+----------+
+    | output1 | output2 | output 3 |
+    +=========+=========+==========+
+    |    25   |    35   |    45    |
+    +---------+---------+----------+
+    |    26   |    36   |    46    |
+    +---------+---------+----------+
+    |    27   |    37   |    47    |
+    +---------+---------+----------+
+
+    This means we make use of ``known future inputs``. This can be achieved using
+    following configuration
+    num_inputs=2, num_outputs=3, lookback=4, forecast_len=3, forecast_step=1, known_future_inputs=True
+
+    The general shape of output/target/label is
+    (examples, num_outputs, forecast_len)
+
+    The general shape of inputs/x is
+    (examples, lookback + forecast_len-1, ....num_inputs)
+
+
+    Examples:
+        >>> import numpy as np
+        >>> from ai4water.utils.utils import prepare_data
+        >>> num_examples = 50
+        >>> dataframe = np.arange(int(num_examples*5)).reshape(-1, num_examples).transpose()
+        >>> dataframe[0:10]
+        array([[  0,  50, 100, 150, 200],
+               [  1,  51, 101, 151, 201],
+               [  2,  52, 102, 152, 202],
+               [  3,  53, 103, 153, 203],
+               [  4,  54, 104, 154, 204],
+               [  5,  55, 105, 155, 205],
+               [  6,  56, 106, 156, 206],
+               [  7,  57, 107, 157, 207],
+               [  8,  58, 108, 158, 208],
+               [  9,  59, 109, 159, 209]])
+        >>> x, prevy, y = prepare_data(dataframe, num_outputs=2, lookback=4,
+        ...    input_steps=2, forecast_step=2, forecast_len=4)
+        >>> x[0]
+        array([[  0.,  50., 100.],
+              [  2.,  52., 102.],
+              [  4.,  54., 104.],
+              [  6.,  56., 106.]], dtype=float32)
+        >>> y[0]
+        array([[158., 159., 160., 161.],
+              [208., 209., 210., 211.]], dtype=float32)
+
+        >>> x, prevy, y = prepare_data(dataframe, num_outputs=2, lookback=4,
+        ...    forecast_len=3, known_future_inputs=True)
+        >>> x[0]
+        array([[  0,  50, 100],
+               [  1,  51, 101],
+               [  2,  52, 102],
+               [  3,  53, 103],
+               [  4,  54, 104],
+               [  5,  55, 105],
+               [  6,  56, 106]])       # (7, 3)
+        >>> # it is important to note that although lookback=4 but x[0] has shape of 7
+        >>> y[0]
+        array([[154., 155., 156.],
+               [204., 205., 206.]], dtype=float32)  # (2, 3)
+    """
+    if not isinstance(data, np.ndarray):
+        if isinstance(data, pd.DataFrame):
+            data = data.values
+        else:
+            raise TypeError(f"unknown data type for data {data.__class__.__name__}")
+
+    if num_inputs is None and num_outputs is None:
+        raise ValueError("""
+Either of num_inputs or num_outputs must be provided.
+""")
+
+    features = data.shape[1]
+    if num_outputs is None:
+        num_outputs = features - num_inputs
+
+    if num_inputs is None:
+        num_inputs = features - num_outputs
+
+#     assert num_inputs + num_outputs == features, f"""
+# num_inputs {num_inputs} + num_outputs {num_outputs} != total features {features}"""
+
+    if len(data) <= 1:
+        raise ValueError(f"Can not create batches from data with shape {data.shape}")
+
+    time_steps = lookback
+    if known_future_inputs:
+        lookback = lookback + forecast_len
+        assert forecast_len > 1, f"""
+            known_futre_inputs should be True only when making predictions at multiple 
+            horizons i.e. when forecast length/number of horizons to predict is > 1.
+            known_future_inputs: {known_future_inputs}
+            forecast_len: {forecast_len}"""
+
+    examples = len(data)
+
+    x = []
+    prev_y = []
+    y = []
+
+    for i in range(examples - lookback * input_steps + 1 - forecast_step - forecast_len * output_steps + 1):
+        stx, enx = i, i + lookback * input_steps
+        x_example = data[stx:enx:input_steps, 0:num_inputs]
+
+        st, en = i, i + (lookback - 1) * input_steps
+        y_data = data[st:en:input_steps, -num_outputs:]
+
+        sty = (i + time_steps * input_steps) + forecast_step - input_steps
+        eny = sty + forecast_len * output_steps
+        if num_outputs == 0:
+            target = np.array([]).reshape(forecast_len, 0)
+        else:
+            target = data[sty:eny:output_steps, -num_outputs:]
+
+        x.append(np.array(x_example))
+        prev_y.append(np.array(y_data))
+        y.append(np.array(target))
+
+    if len(x)<1:
+        raise ValueError(f"""
+        no examples generated from data of shape {data.shape} with lookback 
+        {lookback} input_steps {input_steps} forecast_step {forecast_step} forecast_len {forecast_len}
+""")
+    x = np.stack(x)
+    prev_y = np.array([np.array(i, dtype=np.float32) for i in prev_y], dtype=data.dtype)
+    # transpose because we want labels to be of shape (examples, outs, forecast_len)
+    y = np.array([np.array(i, dtype=np.float32).T for i in y], dtype=data.dtype)
+
+    if mask is not None:
+        if isinstance(mask, np.ndarray):
+            assert mask.ndim == 1
+            assert len(x) == len(mask), f"Number of generated examples are {len(x)} " \
+                                        f"but the length of mask is {len(mask)}"
+        elif isinstance(mask, float) and np.isnan(mask):
+            mask = np.invert(np.isnan(y))
+            mask = np.array([all(i.reshape(-1,)) for i in mask])
+        else:
+            assert isinstance(mask, int), f"""
+                    Invalid mask identifier given of type: {mask.__class__.__name__}"""
+            mask = y != mask
+            mask = np.array([all(i.reshape(-1,)) for i in mask])
+
+        x = x[mask]
+        prev_y = prev_y[mask]
+        y = y[mask]
+
+    return x, prev_y, y
